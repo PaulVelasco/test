d8246ab16e23bdbf73fa0fafdf4868401c4537cd
Paul <Paul@Pauls-Mac-mini.local>
2013-06-11 19:15:19 -0400

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Paul  on 6/11/13.
+//  Copyright (c) 2013 Paul . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
