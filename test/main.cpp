8e90f8fbde53ca3a4fddff8d40a85ee22609d975
Paul Velasco <furo87@hotmail.com>
2013-06-12 22:45:16 -0400

    Revert "Revert "test program""
    This reverts commit 3ad38c716df377d742bad55aa69a2679df4ecd8b.

--- test/main.cpp
+++ test/main.cpp
@@ -3,16 +3,29 @@
 //  test
 //
 //  Created by Paul  on 6/11/13.
-//  Copyright (c) 2013 Paul . All rights reserved.
+//  Copyright (c) 2013 Paul Velasco. All rights reserved.
 //
 
 #include <iostream>
+#include <math.h>
 
-int main(int argc, const char * argv[])
+using namespace std;
+
+int main()
 {
+    int x,y,total;
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    
+    cout<< "Enter a number : "<<endl;
+    cin>> x;
+    cout<<"Enter another number "<<endl;
+    cin>> y;
+    
+    total = (((x /2 )/y) + 4);
+    cout<< "The numbers you enter are: "<<x<<","<<y<<endl;
+    cout <<"the Total is : "<<total<< endl;
+    
     return 0;
 }
 
